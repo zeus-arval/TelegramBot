191218b4af1a92632d7bd1040e5e745a700966cc
Artur Valdna <arturvaldna@gmail.com>
2022-01-20 19:57:52 +0200

    Created a main program file

---
+++ Soft/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Service.Common;
+using System;
+using System.Threading.Tasks;
+
+namespace TelegramBotTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+            host.Start();
+
+
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices(
+                (_, services) =>
+                    services.AddTransient<IService>()
+                );
+    }
+}
